94dfae0f3b7254a9fc2e33124a521210b88665ac
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-08 22:14:58 +0100

    Prepare for 0.02 version

--- ChangePassword.pm
+++ ChangePassword.pm
@@ -12,7 +12,7 @@ use Plack::Session;
 use Tags::HTML::ChangePassword;
 use Tags::HTML::Container;
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 sub _css {
 	my ($self, $env) = @_;
@@ -509,6 +509,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
